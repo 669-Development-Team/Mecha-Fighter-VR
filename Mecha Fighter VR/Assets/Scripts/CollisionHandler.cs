aa04e4f1528a585b906f34b75e13f7031df08097
Jose C <jcastan6@mail.sfsu.edu>
2019-10-13 16:37:53 -0700

    added 2 player scene with side camera for testing

---
+++ Mecha Fighter VR/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+
+{
+    public GameObject gameObject;
+    public Material[] materials;
+    Renderer renderer;
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.sharedMaterial = materials[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision cul)
+    {
+        RobotAnimation controller = gameObject.GetComponent<RobotAnimation>();
+        controller.HandleInput("down a");
+        renderer.sharedMaterial = materials[1];
+
+    }
+    void OnCollisionExit(Collision cul)
+    {
+        renderer.sharedMaterial = materials[0];
+
+    }
+
+}
