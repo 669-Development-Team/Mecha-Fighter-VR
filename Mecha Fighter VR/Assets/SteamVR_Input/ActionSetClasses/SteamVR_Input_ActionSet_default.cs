ef848babf3d59c5e55d1d93e6d4878c1f532208a
AlexWolski <alex.theinfinite@gmail.com>
2019-10-16 15:21:32 -0700

    Updated SteamVR plugin

--- Mecha Fighter VR/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Mecha Fighter VR/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -89,27 +89,19 @@ namespace Valve.VR
             }
         }
         
-        public virtual SteamVR_Action_Vector2 JoystickLeft
+        public virtual SteamVR_Action_Boolean SnapTurnLeft
         {
             get
             {
-                return SteamVR_Actions.default_JoystickLeft;
+                return SteamVR_Actions.default_SnapTurnLeft;
             }
         }
         
-        public virtual SteamVR_Action_Vector2 JoystickRight
+        public virtual SteamVR_Action_Boolean SnapTurnRight
         {
             get
             {
-                return SteamVR_Actions.default_JoystickRight;
-            }
-        }
-        
-        public virtual SteamVR_Action_Vector2 TrackpadLeft
-        {
-            get
-            {
-                return SteamVR_Actions.default_TrackpadLeft;
+                return SteamVR_Actions.default_SnapTurnRight;
             }
         }
         
