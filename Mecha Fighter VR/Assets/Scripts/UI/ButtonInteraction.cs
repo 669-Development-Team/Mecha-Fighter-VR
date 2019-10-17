9477b60826120e468e52b5a268ebb2326521200a
AlexWolski <alex.theinfinite@gmail.com>
2019-10-16 21:41:40 -0700

    Added haptics and made buttons interactable

---
+++ Mecha Fighter VR/Assets/Scripts/UI/ButtonInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInteraction : MonoBehaviour
+{
+    //The materials for when the button is being hovered over or not
+    public Material unselected;
+    public Material selected;
+
+    void OnTriggerEnter(Collider otherObject)
+    {
+        transform.GetComponent<MeshRenderer>().material = selected;
+    }
+
+    private void OnTriggerExit(Collider otherObject)
+    {
+        transform.GetComponent<MeshRenderer>().material = unselected;
+    }
+}
---
+++ Mecha Fighter VR/Assets/Scripts/UI/ControllerInteraction.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class ControllerInteraction : MonoBehaviour
+{
+    //Device that the script is attatched to
+    private SteamVR_Input_Sources source;
+    //Object that controls haptic feedback
+    public SteamVR_Action_Vibration hapticController;
+
+    void Start()
+    {
+        //Get the input source from the parent
+        source = GetComponentInParent<SteamVR_Behaviour_Pose>().inputSource;
+    }
+
+    void OnTriggerEnter(Collider otherObject)
+    {
+        //If the controller enters a UI button, pulse
+        if(otherObject.tag == "UI")
+            pulse(1.0f, source);
+    }
+
+    private void OnTriggerExit(Collider otherObject)
+    {
+        //If the controller exits a UI button, pulse
+        if (otherObject.tag == "UI")
+            pulse(0.1f, source);
+    }
+
+    private void pulse(float amplitude, SteamVR_Input_Sources source)
+    {
+        hapticController.Execute(0, 0.1f, 0.1f, amplitude, source);
+    }
+}
