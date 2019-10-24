6e09212fc80254c869bf16aaf11d49a36022b758
Jose C <jcastan6@mail.sfsu.edu>
2019-10-24 16:13:49 -0700

    added fighter animations -- by Ben

---
+++ Mecha Fighter VR/Assets/Scripts/SetIKWeight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion.FinalIK;
+
+public class SetIKWeight : MonoBehaviour
+{
+    Animator animator;
+
+    public VRIK vrik;
+
+    private IKSolverVR solverVR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        solverVR = (IKSolverVR) vrik.GetIKSolver();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        solverVR.SetIKPositionWeight(animator.GetFloat("IKWeight"));
+    }
+
+    void ApplyRootMotion(int value) {
+        animator.applyRootMotion = value > 0 ? true : false;
+    }
+}
