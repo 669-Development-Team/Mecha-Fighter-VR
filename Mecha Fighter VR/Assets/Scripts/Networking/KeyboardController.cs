4c0dd8f799c660610bd238f09ad0cd6caffc6562
Jose C <jcastan6@mail.sfsu.edu>
2019-11-04 16:58:14 -0800

    Added network controller (Alex)

---
+++ Mecha Fighter VR/Assets/Scripts/Networking/KeyboardController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardController : MonoBehaviour
+{
+    private NetworkPlayerController playerController;
+    private bool leftShieldActive = false;
+    private bool rightShieldActive = false;
+
+    void Start()
+    {
+        playerController = gameObject.GetComponent<NetworkPlayerController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("l"))
+        {
+            if (!leftShieldActive)
+                playerController.enableLeftShield();
+            else
+                playerController.disableLeftShield();
+
+            leftShieldActive = !leftShieldActive;
+        }
+        
+        if (Input.GetKeyDown("r"))
+        {
+            if (!rightShieldActive)
+                playerController.enableRightShield();
+            else
+                playerController.disableRightShield();
+
+            rightShieldActive = !rightShieldActive;
+        }
+
+        if (Input.GetKeyDown("p"))
+            playerController.activateProjectile();
+
+        if (Input.GetKeyDown("u"))
+            playerController.activateUppercut();
+
+        if (Input.GetKeyDown("g"))
+            playerController.activateGroundPound();
+    }
+}
---
+++ Mecha Fighter VR/Assets/Scripts/Networking/NetworkPlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Action;
+
+public class NetworkPlayerController : MonoBehaviour
+{
+    //The ability scripts attatched to the opponent
+    private ShieldAbility shieldScript;
+    private ProjectileAbility projectileScript;
+    private UppercutAbility uppercutScript;
+    private GroundPoundAbility groundPoundScipt;
+
+    private void Start()
+    {
+        //Get references to all of the ability scripts
+        shieldScript = gameObject.GetComponent<ShieldAbility>();
+        projectileScript = gameObject.GetComponent<ProjectileAbility>();
+        uppercutScript = gameObject.GetComponent<UppercutAbility>();
+        groundPoundScipt = gameObject.GetComponent<GroundPoundAbility>();
+    }
+
+    public void enableLeftShield()
+    {
+        shieldScript.ToggleLeftShield(true);
+    }
+
+    public void disableLeftShield()
+    {
+        shieldScript.ToggleLeftShield(false);
+    }
+
+    public void enableRightShield()
+    {
+        shieldScript.ToggleRightShield(true);
+    }
+
+    public void disableRightShield()
+    {
+        shieldScript.ToggleRightShield(false);
+    }
+
+    public void activateProjectile()
+    {
+        projectileScript.ActivateAbility();
+    }
+
+    public void activateUppercut()
+    {
+        uppercutScript.ActivateAbility(null);
+    }
+
+    public void activateGroundPound()
+    {
+        groundPoundScipt.ActivateAbility(null);
+    }
+}
