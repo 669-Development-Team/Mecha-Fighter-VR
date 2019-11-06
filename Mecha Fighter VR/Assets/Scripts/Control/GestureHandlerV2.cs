d8c5f976473edde570a1b5122419712184a6efea
Jose C <jcastan6@mail.sfsu.edu>
2019-11-05 16:38:03 -0800

    Added gesturehandlerv2 (ben)

---
+++ Mecha Fighter VR/Assets/Scripts/Control/GestureHandlerV2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureHandlerV2 : MonoBehaviour
+{
+    public GestureTrackingNode leftTracker;
+    public GestureTrackingNode rightTracker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rightTracker.GetGesture(Gestures.UP) && !leftTracker.GripHeldDown())
+        {
+
+        }
+    }
+
+    IEnumerator awaitGesture(GestureTrackingNode tracker, Gestures gesture)
+    {
+        while (tracker.GripHeldDown())
+        {
+            yield return null;
+        }
+
+        if (tracker.GetGesture(gesture))
+        {
+
+        }
+    }
+}
