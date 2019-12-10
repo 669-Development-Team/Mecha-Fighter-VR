ce2d53e048c8a4577e2d31db3a7bd2973f98cace
AlexWolski <alex.theinfinite@gmail.com>
2019-12-10 00:14:29 -0800

    Added larger arms

---
+++ Mecha Fighter VR/Assets/Scripts/ArmSize.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmSize : MonoBehaviour
+{
+    [SerializeField] private float armScale = 1f;
+    [SerializeField] private GameObject leftArm;
+    [SerializeField] private GameObject rightArm;
+
+    void Start()
+    {
+        leftArm.transform.localScale = new Vector3(armScale, armScale, armScale);
+        rightArm.transform.localScale = new Vector3(armScale, armScale, armScale);
+    }
+
+    public float getScale()
+    {
+        return armScale;
+    }
+}
---
+++ Mecha Fighter VR/Assets/Scripts/Control/ScaleHandPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleHandPosition : MonoBehaviour
+{
+    [SerializeField] private GameObject playerRoot;
+    [SerializeField] private GameObject headTarget;
+    [Tooltip("How far away the chest is from the head")]
+    [SerializeField] private Vector3 chestDisplacement;
+    private ArmSize armSizeScript;
+    private float armSize;
+
+    void Start()
+    {
+        armSize = playerRoot.GetComponent<ArmSize>().getScale();
+    }
+
+    void LateUpdate()
+    {
+        //The displacement vector between the chest and the hand
+        Vector3 displacement = transform.position - headTarget.transform.position;
+        //Scale the displacement by the size of the arms
+        displacement *= armSize;
+        //Add the displacement back on to the arm position
+        transform.position = headTarget.transform.position + displacement;
+    }
+}
